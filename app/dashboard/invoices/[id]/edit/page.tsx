25060dd259c67b9aff6bf9ccefe4bd80979c0465
MohGuma <gomaam14@gamil.com>
2025-07-28 23:12:33 +0400

    update the invocie with CRUD operation through server action form

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,35 @@
+type InvoiceEditProps = {
+  params: Promise<{id: string}>
+}
+
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+
+
+const InvoiceEdit = async ({params}: InvoiceEditProps) => {
+  const {id} = await params;
+
+  const [invoice, customers] =  await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers()
+  ])
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  )
+}
+
+export default InvoiceEdit
